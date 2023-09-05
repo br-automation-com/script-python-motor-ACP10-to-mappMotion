cfea939077925afa619ceb49df656f9c2af41c67
Stephan Stricker <stephan.stricker@br-automation.com>
2023-09-05 16:34:57 +0200

    Added dependencies

--- start.py
+++ start.py
@@ -5,11 +5,43 @@ import datetime
 
 version = 1
 debug_mode = 0
+variable_name = 'AxisMotorCfg.'
 motor_type_undefined = 0
 motor_type_induction = 1  
 motor_type_synchronous = 2  
 
+def evaluate_attrib(variable_name, rootConversion, elem, rootDependencies, dependencies):
+    entry = ''
+
+    # check in entry has a dependency
+    if 'Dependency' in rootConversion.find(elem.attrib['Name']).attrib:
+        dependency = rootConversion.find(elem.attrib['Name']).attrib['Dependency'].split(';')
+        # check all dependencies
+        for element in dependency:
+            # make sure dependency was not already activated
+            if not element in dependencies:
+                entry += variable_name + rootDependencies.find(element).attrib['Entry']+ '\n\t'
+                dependencies.append(element)
+
+    if rootConversion.find(elem.attrib['Name']).attrib['Entry'].startswith('//'):
+        entry += rootConversion.find(elem.attrib['Name']).attrib['Entry']
+    else:
+        entry += variable_name + rootConversion.find(elem.attrib['Name']).attrib['Entry']
+
+    return entry
+
 def convert_apt_files():
+    # --------------------------------------------------------------------------
+    # open conversion and dependencies xml file
+    treeConversionSync = ET.parse('conversion_sync.xml')
+    rootConversionSync = treeConversionSync.getroot()
+    treeConversionInd = ET.parse('conversion_ind.xml')
+    rootConversionInd = treeConversionInd.getroot()
+    treeDependenciesSync = ET.parse('dependencies_sync.xml')
+    rootDependenciesSync = treeDependenciesSync.getroot()
+    treeDependenciesInd = ET.parse('dependencies_ind.xml')
+    rootDependenciesInd = treeDependenciesInd.getroot()
+
     # get all files in the apt folder
     for file in os.listdir('apt'):
         # Nur Dateien ausgeben, die die gewünschte Endung haben
@@ -33,15 +65,9 @@ def convert_apt_files():
             treeMotor = ET.parse('apt/' + file)
             rootMotor = treeMotor.getroot()
 
-            # --------------------------------------------------------------------------
-            # open conversion xml file
-            treeConversionSync = ET.parse('conversion_sync.xml')
-            rootConversionSync = treeConversionSync.getroot()
-            treeConversionInd = ET.parse('conversion_ind.xml')
-            rootConversionInd = treeConversionInd.getroot()
-
             # set default parameter
             motor_type = motor_type_undefined
+            dependencies = []
 
             # --------------------------------------------------------------------------
             # find motor type
@@ -82,18 +108,20 @@ def convert_apt_files():
 
                     if 'Name' in elem.attrib and elem.attrib['Name'] != 'MOTOR_TYPE' and 'Value' in elem.attrib and 'ID' in elem.attrib: 
                         try:
+                            entry = ''
+
                             # find entry for induction motor
-                            if motor_type == motor_type_induction:
-                                entry = rootConversionInd.find(elem.attrib['Name']).attrib['Entry']
+                            if motor_type == motor_type_induction and rootConversionInd.find(elem.attrib['Name']) != None:
+                               entry = evaluate_attrib(variable_name + 'ParMotorInduction.', rootConversionInd, elem, rootDependenciesInd, dependencies)
 
                             # find entry for synchronous motor
-                            elif motor_type == motor_type_synchronous:                            
-                                entry = rootConversionSync.find(elem.attrib['Name']).attrib['Entry']
+                            elif motor_type == motor_type_synchronous and rootConversionSync.find(elem.attrib['Name']) != None:
+                                entry = evaluate_attrib(variable_name + 'ParMotorSynchronous.', rootConversionSync, elem, rootDependenciesSync, dependencies)
 
                             # parameter was not found
                             if entry == '':
                                 new_row = '// ERROR: ' + elem.attrib['Name'] + ' not found in conversion table\n'
-                                print(new_row.replace('// ', '-->'))
+                                print(new_row.replace('// ', '-->').replace('\n', ''))
 
                             # parameter was found
                             else:
